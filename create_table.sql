8ddad9053b8f746d2847ffb899c8a5cee5adae31
Greenbirch2007 <291109028@qq.com>
2018-09-12 15:35:59 +0800

    把sql脚本找回来了

---
+++ create_table.sql
@@ -0,0 +1,54 @@
+
+
+create table j_stocks(
+id int not null primary key auto_increment,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
+
+select * from j_stocks;
+
+
+create table a_stocks(
+id int not null primary key auto_increment,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
+
+
+create table hk_stocks(
+id int not null primary key auto_increment,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
+
+
+
+create table us_stocks(
+id int not null primary key auto_increment,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
+
+
+
+
+create table bond_bankStock(
+id int not null primary key auto_increment,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
+
+# 增加了首次插入字段的时间记录
+create table A50_myStock(
+id int not null primary key auto_increment,
+TM timestamp default current_timestamp ,
+indexs varchar(11),
+stock varchar(11),
+spread varchar(11)
+) engine=InnoDB  charset=utf8;
